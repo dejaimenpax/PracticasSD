ed0992170cb43c8d60aab3e36d858327ac72f59d
Sr.Fernandez <e.fernandezor.2017@alumnos.urjc.es>
2022-03-26 12:02:57 +0100

    Create a project

---
+++ src/test/java/es/Group3/BiciURJC/BiciUrjcApplicationTests.java
@@ -0,0 +1,13 @@
+package es.Group3.BiciURJC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiciUrjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
