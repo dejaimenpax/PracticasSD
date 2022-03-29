7734f3d978a2c504be0646b2fb70ca0f26dc8701
dejaimenpax <jaimegonzalezcasero@gmail.com>
2022-03-29 12:40:15 +0200

    Separate user from GestionUsuarios

--- src/main/java/model/GestionUsuarios.java
+++ src/main/java/model/GestionUsuarios.java
@@ -1,70 +1,10 @@
 package model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Hashtable;
 
 public class GestionUsuarios {
 
     public Hashtable<String, Usuario> users = new Hashtable<>();
-
-    protected class Usuario {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private String id;
-
-        private String password;
-        private String fullName;
-        private String entryDate;
-        private boolean state;
-        //private BufferedImage BufferedImage photo;
-
-        private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-
-        public String getId() {
-            return id;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getFullName() {
-            return fullName;
-        }
-
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-        }
-
-        public String getEntryDate() {
-            return entryDate;
-        }
-
-        public boolean isState() {
-            return state;
-        }
-
-        public void setState(boolean state) {
-            this.state = state;
-        }
-
-        public Usuario(String id, String password, String fullName){
-            this.id = id;
-            this.password = password;
-            this.fullName = fullName;
-            this.entryDate = dtf.format(LocalDate.now());
-            state = true;
-        }
-
-    }
 
     public void removeUser(Usuario us){
         us.setState(false);
---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,71 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+public class Usuario{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String password;
+    private String fullName;
+    private String entryDate;
+    private boolean state;
+    //private BufferedImage BufferedImage photo;
+
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+
+    protected Usuario() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEntryDate() {
+        return entryDate;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public Usuario(String id, String password, String fullName){
+        this.id = id;
+        this.password = password;
+        this.fullName = fullName;
+        this.entryDate = dtf.format(LocalDate.now());
+        state = true;
+    }
+
+    @Override
+    public String toString(){
+        return("");
+    }
+
+}
