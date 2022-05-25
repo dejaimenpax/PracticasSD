e92086e5a4c327d24cf9cc2f791f7f697986fb25
dejaimenpax <jaimegonzalezcasero@gmail.com>
2022-05-25 09:57:13 +0200

    Creo que está todo

--- src/main/java/es/Group3/BiciURJC/controller/ReservaDevolucionRestController.java
+++ src/main/java/es/Group3/BiciURJC/controller/ReservaDevolucionRestController.java
@@ -4,10 +4,15 @@ package es.Group3.BiciURJC.controller;
 import es.Group3.BiciURJC.Service.BicicletaService;
 import es.Group3.BiciURJC.Service.EstacionService;
 import es.Group3.BiciURJC.Service.GestionReservasDecoluciones;
+import es.Group3.BiciURJC.controller.exceptions.IncorrectStationCapacity;
 import es.Group3.BiciURJC.controller.exceptions.InsufficentBalance;
 import es.Group3.BiciURJC.model.*;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -44,10 +49,31 @@ public class ReservaDevolucionRestController {
 		return bicicletas.findAll();
 	}
 
-	@PutMapping("/")
-	public ResponseEntity<Bicicleta> reserve(@RequestBody CapsulaIds identificadores) {
+	@PutMapping("/bicycles/{id}")
+	@Operation(summary = "Reservar bicicleta")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Bicicleta reservada",
+					content = {@Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation=Estacion.class)
+					)}
+			),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Esta bicicleta no se puede reservar",
+					content = @Content
+			),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Recursos no encontrados",
+					content = @Content
+			)
+	})
+	public ResponseEntity<Bicicleta> reserve(@PathVariable Long id, @RequestBody CapsulaIds identificadores) {
 		Optional<Estacion> aux_st = estaciones.findOne(identificadores.getStation_id());
-		Optional<Bicicleta> aux_bici = bicicletas.findOne(identificadores.getBicycle_id());
+		Optional<Bicicleta> aux_bici = bicicletas.findOne(id);
 
 		if (aux_st.isPresent() && aux_bici.isPresent()) {
 
@@ -62,6 +88,7 @@ public class ReservaDevolucionRestController {
 			if(saldo >=3) {
 				if(estacion.getEstado() == EstadoEstacion.ACTIVA && bici.getEstacion().getId().equals(estacion.getId()) && bici.getEstado().getLista().getFirst() == EstadoBicicleta.EN_BASE){
 					double new_saldo = data.get("saldo").asDouble()-3;
+					aux_bici.get().getEstado().getLista().removeFirst();
 					aux_bici.get().getEstado().getLista().addFirst(EstadoBicicleta.RESERVADA);
 					aux_bici.get().setEstacion(null);
 					aux_st.get().deleteBike(aux_bici.get());
@@ -78,6 +105,64 @@ public class ReservaDevolucionRestController {
 				throw new InsufficentBalance("Saldo insuficiente");
 		} else
 			return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/stations/{id}")
+	@Operation(summary = "Devolver bicicleta")
+	@ApiResponses(value = {
+			@ApiResponse(
+					responseCode = "200",
+					description = "Bicicleta devuelta a una estación",
+					content = {@Content(
+							mediaType = "application/json",
+							schema = @Schema(implementation=Estacion.class)
+					)}
+			),
+			@ApiResponse(
+					responseCode = "400",
+					description = "Esta bicicleta no se puede devolver a la estación especificada",
+					content = @Content
+			),
+			@ApiResponse(
+					responseCode = "404",
+					description = "Recursos no encontrados",
+					content = @Content
+			)
+	})
+	public ResponseEntity<Bicicleta> returnBicycle(@PathVariable Long id, @RequestBody CapsulaIds identificadores){
+		Optional<Estacion> aux_st = estaciones.findOne(id);
+		Optional<Bicicleta> aux_bici = bicicletas.findOne(identificadores.getBicycle_id());
+		if (aux_st.isPresent() && aux_bici.isPresent()) {
+			Estacion estacion= aux_st.get();
+			Bicicleta bici= aux_bici.get();
+			if(estacion.getEstado() == EstadoEstacion.ACTIVA && bici.getEstado().getLista().getFirst() == EstadoBicicleta.RESERVADA) {
+				if(estacion.getSize()>estacion.getListaBicis().size()) {
+					RestTemplate plantilla = new RestTemplate();
+					String url = "http://localhost:8081/users/" + identificadores.getUser_id();
+					ObjectNode data = plantilla.getForObject(url, ObjectNode.class);
+					assert data != null;
+					double new_saldo = data.get("saldo").asDouble()+ 1.5;
+					aux_bici.get().getEstado().getLista().removeFirst();
+					aux_bici.get().getEstado().getLista().addFirst(EstadoBicicleta.EN_BASE);
+					aux_bici.get().setEstacion(aux_st.get());
+					aux_st.get().addBici(aux_bici.get());
+					estaciones.save(aux_st.get());
+					bicicletas.save(aux_bici.get());
+					data.put("saldo", new_saldo);
+					plantilla.put(url,data);
+					URI location = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+					return ResponseEntity.created(location).build();
+				}
+				else
+					throw new IncorrectStationCapacity("La estación no admite más bicicletas");
+			}
+			else {
+				return ResponseEntity.unprocessableEntity().build();
+			}
+		}
+		else
+			return ResponseEntity.notFound().build();
+
 	}
 
 
